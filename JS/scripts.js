fa5988f2e5b012d9c272e79a9d54ff9636473991
WagnerGeroldi <wagnergeroldi@gmail.com>
2021-11-24 17:16:38 -0300

    teste slider

--- JS/scripts.js
+++ JS/scripts.js
@@ -6,8 +6,6 @@ function toggleMenu(e) {
   if (e.type === "touchstart") e.preventDefault(); // previne o clique no touch
   nav.classList.toggle("active");
 
-  
-
   //melhorar acessibilidade do menu
   const active = nav.classList.contains("active"); // se tiver active é true, senao é falso
   e.currentTarget.setAttribute("aria-expanded", active); // assim ele muda o aria para true e false
@@ -19,9 +17,8 @@ function toggleMenu(e) {
 }
 
 function closeMenuMobile() {
-  if(nav.classList.contains('active') === true) {
-    
-    nav.classList.remove("active")
+  if (nav.classList.contains("active") === true) {
+    nav.classList.remove("active");
   }
 }
 
@@ -29,49 +26,54 @@ function closeMenuMobile() {
 btnMobile.addEventListener("click", toggleMenu);
 btnMobile.addEventListener("touchstart", toggleMenu); //melhora desempenho no touch
 
-
 // fim menu responsivo
 
 //funcao typewriter
 function typeWriter(elemento) {
-  const textoArray = elemento.innerHTML.split('');
-  elemento.innerHTML = '';
+  const textoArray = elemento.innerHTML.split("");
+  elemento.innerHTML = "";
   textoArray.forEach((letra, i) => {
-    setTimeout(() => elemento.innerHTML += letra, 75 * i);
+    setTimeout(() => (elemento.innerHTML += letra), 75 * i);
   });
 }
 
-
-const titulo = document.querySelector('h1');
+const titulo = document.querySelector("h1");
 typeWriter(titulo);
 //fim typewriter
 
 
-//slider
-var slider = new KeenSlider("#my-keen-slider", {
-  slidesPerView: 3,
-  mode: "free-snap",
-  spacing: 15,
-  loop: true,
-})
 
-const buttonleiamais = document.querySelector('#leia-mais')
-const classemostrar = document.querySelector('.hidden')
+window.addEventListener("resize", function () {
+  var largura = window.innerWidth;
+  var slider = new KeenSlider("#my-keen-slider", {
+    slidesPerView: 3,
+    mode: "free-snap",
+    spacing: 15,
+    loop: true,
+  });
 
+  if (largura < 750)
+    slider = new KeenSlider("#my-keen-slider", {
+      slidesPerView: 2,
+      mode: "free-snap",
+      spacing: 15,
+      loop: true,
+    });
+});
 
+const buttonleiamais = document.querySelector("#leia-mais");
+const classemostrar = document.querySelector(".hidden");
 
 function mostraritem() {
-  classemostrar.classList.remove('hidden')
-  buttonleiamais.classList.add('hidden')
+  classemostrar.classList.remove("hidden");
+  buttonleiamais.classList.add("hidden");
 }
-buttonleiamais.addEventListener('click', mostraritem)
+buttonleiamais.addEventListener("click", mostraritem);
 
-
-const buttonesconder = document.querySelector('.button-esconder')
-
+const buttonesconder = document.querySelector(".button-esconder");
 
 function escondeitem() {
-  classemostrar.classList.add('hidden')
-  buttonleiamais.classList.remove('hidden')
+  classemostrar.classList.add("hidden");
+  buttonleiamais.classList.remove("hidden");
 }
-buttonesconder.addEventListener('click', escondeitem)
+buttonesconder.addEventListener("click", escondeitem);
