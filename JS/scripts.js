589c12b3235c249cd0145191afd9d867f6f96236
WagnerGeroldi <wagnergeroldi@gmail.com>
2021-11-25 16:36:06 -0300

    add botao scrool

--- JS/scripts.js
+++ JS/scripts.js
@@ -79,3 +79,23 @@ function hiddenTextAbout() {
   btnReadMore.classList.remove("hidden");
 }
 btnHiddenText.addEventListener("click", hiddenTextAbout);
+
+
+//botao subir
+
+$(document).ready(function () {
+  // Exibe ou oculta o botão
+  $(window).scroll(function () {
+      if ($(this).scrollTop() > 200) {
+          $('.voltar-ao-topo').fadeIn(200);
+      } else {
+          $('.voltar-ao-topo').fadeOut(200);
+      }
+  });
+
+  // Faz animação para subir
+  $('.voltar-ao-topo').click(function (event) {
+      event.preventDefault();
+      $('html, body').animate({ scrollTop: 0 }, 300);
+  })
+});
